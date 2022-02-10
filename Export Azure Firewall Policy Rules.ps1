c5b3a2f001c03753a61d08771dc90dff2ad5e2ba
Pantelis Apostolidis <proximagr@hotmail.com>
2022-02-10 10:04:08 +0200

    Add files via upload

---
+++ Export Azure Firewall Policy Rules.ps1
@@ -0,0 +1,29 @@
+﻿$fp = Get-AzFirewallPolicy -Name azfwpolicy -ResourceGroupName azurehub
+$rcg = Get-AzFirewallPolicyRuleCollectionGroup -Name DefaultNetworkRuleCollectionGroup -AzureFirewallPolicy $fp
+
+$returnObj = @()
+foreach ($rulecol in $rcg.Properties.RuleCollection) {
+
+foreach ($rule in $rulecol.rules)
+{
+$properties = [ordered]@{
+    RuleCollectionName = $rulecol.Name;
+    RulePriority = $rulecol.Priority;
+    ActionType = $rulecol.Action.Type;
+    RUleConnectionType = $rulecol.RuleCollectionType;
+    Name = $rule.Name;
+    protocols = $rule.protocols -join ", ";
+    SourceAddresses = $rule.SourceAddresses -join ", ";
+    DestinationAddresses = $rule.DestinationAddresses -join ", ";
+    SourceIPGroups = $rule.SourceIPGroups -join ", ";
+    DestinationIPGroups = $rule.DestinationIPGroups -join ", ";
+    DestinationPorts = $rule.DestinationPorts -join ", ";
+    DestinationFQDNs = $rule.DestinationFQDNs -join ", ";
+}
+$obj = New-Object psobject -Property $properties
+$returnObj += $obj
+}
+
+$returnObj | Export-Csv c:\temp\rules.csv -NoTypeInformation
+}
+
---
+++ Import Azure Firewall Policy Rules.ps1
@@ -0,0 +1,49 @@
+﻿$targetfp = Get-AzFirewallPolicy -Name testpolicy -ResourceGroupName azurehub
+$targetrcg = New-AzFirewallPolicyRuleCollectionGroup -Name DefaultNetworkRuleCollectionGroup -Priority 200 -FirewallPolicyObject $targetfp
+
+$RulesfromCSV = @()
+$readObj = import-csv C:\temp\rules.csv
+foreach ($entry in $readObj)
+{
+    $properties = [ordered]@{
+        RuleCollectionName = $entry.RuleCollectionName;
+        RulePriority = $entry.RulePriority;
+        ActionType = $entry.ActionType;
+        Name = $entry.Name;
+        protocols = $entry.protocols -split ", ";
+        SourceAddresses = $entry.SourceAddresses -split ", ";
+        DestinationAddresses = $entry.DestinationAddresses -split ", ";
+        SourceIPGroups = $entry.SourceIPGroups -split ", ";
+        DestinationIPGroups = $entry.DestinationIPGroups -split ", ";
+        DestinationPorts = $entry.DestinationPorts -split ", ";
+        DestinationFQDNs = $entry.DestinationFQDNs -split ", ";
+    }
+    $obj = New-Object psobject -Property $properties
+    $RulesfromCSV += $obj
+}
+
+$RulesfromCSV
+Clear-Variable rules
+$rules = @()
+foreach ($entry in $RulesfromCSV)
+{
+    $RuleParameter = @{
+        Name = $entry.Name;
+        Protocol = $entry.protocols
+        sourceAddress = $entry.SourceAddresses
+        DestinationAddress = $entry.DestinationAddresses
+        DestinationPort = $entry.DestinationPorts
+    }
+    $rule = New-AzFirewallPolicyNetworkRule @RuleParameter
+    $NetworkRuleCollection = @{
+        Name = $entry.RuleCollectionName
+        Priority = $entry.RulePriority
+        ActionType = $entry.ActionType
+        Rule       = $rules += $rule
+    }
+}
+
+# Create a network rule collection
+$NetworkRuleCategoryCollection = New-AzFirewallPolicyFilterRuleCollection @NetworkRuleCollection
+# Deploy to created rule collection group
+Set-AzFirewallPolicyRuleCollectionGroup -Name $targetrcg.Name -Priority 200 -RuleCollection $NetworkRuleCategoryCollection -FirewallPolicyObject $targetfp
